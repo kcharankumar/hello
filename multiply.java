950951e92bd87d69b58d41d8af5e8187302410cf
K.Charan Kumar <kcharankumar@gmail.com>
2024-12-12 14:46:17 +0530

    Multiply.java is added

---
+++ multiply.java
@@ -0,0 +1,13 @@
+public class Multiply{
+
+	public static void main (String args[]){
+	
+	  int a = 10, b = 20, result;
+
+	  result = a * b;
+
+	  System.out.println ("Result is : "+result);
+
+  }
+
+}
