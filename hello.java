0423edac5daabeff07bd313d4568e37c4f5b9936
kcharankumar <47662435+kcharankumar@users.noreply.github.com>
2026-01-12 10:56:21 +0530

    Update hello.java

--- hello.java
+++ hello.java
@@ -1,7 +1,7 @@
 public class hello{
   public static void main (String args[]){
 
-   System.out.println ("Lets go to pondicherry..");
+   System.out.println ("I want to visit Switzerland and Queens town..");
 
 
 }
